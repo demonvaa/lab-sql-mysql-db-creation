aae7eb25ba63bf37af6ad62934b85fd85c235a16
demonvaa <ernestovaamonde@yahoo.es>
2026-04-28 16:17:35 +0200

    Modificacion 28022026 16.14

--- create.sql
+++ create.sql
@@ -2,50 +2,53 @@ CREATE DATABASE IF NOT EXISTS lab_mysql;
 USE lab_mysql;
 
 -- TABLA CARS--
-DROP TABLE IF EXISTS Cars;
-CREATE TABLE Cars (
-    VIN VARCHAR(20) NOT NULL, # AQUI NO SE USA INCREMENT POR QUE AUNQUE SEA LA PRIMARY KEY NO ES INT
+DROP TABLE IF EXISTS cars;
+CREATE TABLE cars (
+	id INT AUTO_INCREMENT NOT NULL,
+    VIN VARCHAR(20) NOT NULL, 
     manufacturer VARCHAR(50) NOT NULL,
     model VARCHAR(50) NOT NULL,
     year INT NOT NULL,
     color VARCHAR(30) NOT NULL,
-    PRIMARY KEY (VIN) # 1 POR TABLA
+    PRIMARY KEY (id) 
 );
 
 -- TABLA CUSTOMERS--
-DROP TABLE IF EXISTS Customers;
-CREATE TABLE Customers (
-    customer_ID INT AUTO_INCREMENT NOT NULL, # SOLO SE USA EN LA QUE VA A SER PRIMARY KEY Y INTEGUER
+DROP TABLE IF EXISTS customers;
+CREATE TABLE customers (
+	id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(100) NOT NULL,
     phone_number VARCHAR(20) NOT NULL,
-    email VARCHAR(100) NULL,
+    email VARCHAR(100),
     address VARCHAR(100) NOT NULL,
     city VARCHAR(50) NOT NULL,
     state_province VARCHAR(50) NOT NULL,
     country VARCHAR(50) NOT NULL,
     zip_postal_code VARCHAR(100) NOT NULL,
-    PRIMARY KEY (customer_ID) # 1 POR TABLA
+    PRIMARY KEY (id) # 1 POR TABLA
 );
 
 -- TABLA SALESPERSONS--
-DROP TABLE IF EXISTS Salespersons;
-CREATE TABLE Salespersons (
-    staff_ID INT AUTO_INCREMENT NOT NULL, # SOLO SE USA EN LA QUE VA A SER PRIMARY KEY
+DROP TABLE IF EXISTS salespersons;
+CREATE TABLE salespersons (
+	id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(100) NOT NULL,
     store VARCHAR(100) NOT NULL,
-    PRIMARY KEY (staff_ID) # 1 POR TABLA
+    PRIMARY KEY (id) # 1 POR TABLA
 );
 
 -- TABLA INVOICES
-DROP TABLE IF EXISTS Invoices;
-CREATE TABLE Invoices (
-    invoice_number INT AUTO_INCREMENT NOT NULL, # SOLO SE USA EN LA QUE VA A SER PRIMARY KEY
+DROP TABLE IF EXISTS invoices;
+CREATE TABLE invoices (
+	id INT AUTO_INCREMENT NOT NULL,
+    invoice_number INT  NOT NULL,
     date DATETIME NOT NULL,
-    car_VIN VARCHAR(20) NOT NULL,
-    customer_ID INT NOT NULL,
-    staff_ID INT NOT NULL,
-    PRIMARY KEY (invoice_number), # 1 POR TABLA
-    FOREIGN KEY (car_VIN) REFERENCES Cars(VIN),
-    FOREIGN KEY (customer_ID) REFERENCES Customers(customer_ID),
-    FOREIGN KEY (staff_ID) REFERENCES Salespersons(staff_ID)
+    car_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    staff_id INT NOT NULL,
+    PRIMARY KEY (id), 
+    FOREIGN KEY (car_id) REFERENCES cars(id),
+	FOREIGN KEY (customer_id) REFERENCES customers(id),
+	FOREIGN KEY (staff_id) REFERENCES salespersons(id)
+
 );
--- seedings.sql
+++ seedings.sql
@@ -5,54 +5,54 @@ VALUES
 ('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White'),
 ('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver'),
 ('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray'),
-('DAM41UDN3CHU2WVF7', 'Volvo', 'V60 Cross Country', 2019, 'Gray'); # va a fallar por que VIN es PRIMARY KEY Y ESTA DUPLICADO
+('DAM41UDN3CHU2WVF7', 'Volvo', 'V60 Cross Country', 2019, 'Gray');
 
 INSERT INTO customers 
-(customer_ID, name, phone_NUMBER, email, address, city, state_province, country, zip_postal_code)
+(name, phone_number, email, address, city, state_province, country, zip_postal_code)
 VALUES
-(10001, 'Pablo Picasso', '+34 636 17 63 82', NULL , 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
-(20001, 'Abraham Lincoln', '+1 305 907 7086', NULL , '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
-(30001, 'Napoléon Bonaparte', '+33 1 79 75 40 00', NULL , '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+('Pablo Picasso', '+34 636 17 63 82', NULL , 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+( 'Abraham Lincoln', '+1 305 907 7086', NULL , '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
+( 'Napoléon Bonaparte', '+33 1 79 75 40 00', NULL , '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
 
 # Si tu tabla Customers tiene AUTO_INCREMENT , No te dejará insertar IDs manuales.
 # VA A DAR FALLO POR EL - de email
 
-INSERT INTO salespersons (staff_id, name, store)
+INSERT INTO salespersons (name, store)
 VALUES
-(00001, 'Petey Cruiser', 'Madrid'),
-(00002, 'Anna Sthesia', 'Barcelona'),
-(00003, 'Paul Molive', 'Berlin'),
-(00004, 'Gail Forcewind', 'Paris'),
-(00005, 'Paige Turner', 'Mimia'),
-(00006, 'Bob Frapples', 'Mexico City'),
-(00007, 'Walter Melon', 'Amsterdam'),
-(00008, 'Shonda Leer', 'São Paulo');
+('Petey Cruiser', 'Madrid'),
+('Anna Sthesia', 'Barcelona'),
+('Paul Molive', 'Berlin'),
+('Gail Forcewind', 'Paris'),
+('Paige Turner', 'Mimia'),
+( 'Bob Frapples', 'Mexico City'),
+( 'Walter Melon', 'Amsterdam'),
+( 'Shonda Leer', 'São Paulo');
 
 # Si tu tabla "Salespersons" tiene AUTO_INCREMENT , No te dejará insertar IDs manuales.
 
 
-INSERT INTO invoices (invoice_number, date, car_VIN, customer_ID, staff_ID)
+INSERT INTO invoices (invoice_number, date, car_id, customer_id, staff_id)
 VALUES
 (
   852399038,
   '2018-08-22',
-  (SELECT VIN FROM cars WHERE id = 1),
-  (SELECT customer_ID FROM customers WHERE id = 0),
-  (SELECT staff_ID FROM salespersons WHERE id = 3)
+  (SELECT id FROM cars WHERE id = 1),
+  (SELECT customer_id FROM customers WHERE id = 1),
+  (SELECT staff_id FROM salespersons WHERE id = 3)
 ),
 (
   731166526,
   '2018-12-31',
-  (SELECT VIN FROM cars WHERE id = 3),
-  (SELECT customer_ID FROM customers WHERE id = 2),
-  (SELECT staff_ID FROM salespersons WHERE id = 5)
+  (SELECT id FROM cars WHERE id = 3),
+  (SELECT customer_id FROM customers WHERE id = 2),
+  (SELECT staff_id FROM salespersons WHERE id = 5)
 ),
 (
   271135104,
   '2019-01-22',
-  (SELECT VIN FROM cars WHERE id = 2),
-  (SELECT customer_ID FROM customers WHERE id = 1),
-  (SELECT staff_ID FROM salespersons WHERE id = 7)
+  (SELECT id FROM cars WHERE id = 2),
+  (SELECT customer_id FROM customers WHERE id = 3),
+  (SELECT staff_id FROM salespersons WHERE id = 7)
 );
 	
 
